22ab738deb302934679073bfc903ff375882d8de
Jeremy <jeremy@jeremywoolley.com>
2026-08-25 15:57:22 -0400

    web site

--- api/submit/[path].ts
+++ api/submit/[path].ts
@@ -3,9 +3,10 @@ import { createFerry } from "@jeremy46231/ferry";
 const ferry = createFerry({ basePath: "/api/submit" });
 
 export default {
-  async fetch(request: Request): Promise<Response> {
-    return (
-      (await ferry.handle(request)) ?? new Response("Not found", { status: 404 })
-    );
-  },
+	async fetch(request: Request): Promise<Response> {
+		return (
+			(await ferry.handle(request)) ??
+			new Response("Not found", { status: 404 })
+		);
+	},
 };
